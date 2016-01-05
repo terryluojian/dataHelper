45e6ae0d78f1d1fd7807ea3ba56c19a561cdfd3a
jianluo <jian.luo@fictiongame.com>
2016-01-05 17:26:35 +0800

    plist read and write

--- dataHelper/dataHelper/ViewController.swift
+++ dataHelper/dataHelper/ViewController.swift
@@ -23,19 +23,47 @@ class ViewController: UIViewController {
             let headers = csv.headers  //=> ["id", "name", "age"]
             let a = csv.rows[0]    //=> ["id": "1", "name": "Alice", "age": "18"]
             let b = csv.rows[1]      //=> ["id": "2", "name": "Bob", "age": "19"]
-            print("\n\(headers)\(a)\n\(b)")
+            //print("\n\(headers)\(a)\n\(b)")
+            plistSave("testPlist", content: rows )
+            
             // Columns
             let columns = csv.columns
             let id = csv.columns["id"]  //=> ["Alice", "Bob", "Charlie"]
             let c = csv.columns["content"]    //=> ["18", "19", "20"]
-            print("\n\(columns)\(id)\n\(c)")
+            print("无视\n\(columns)\(id)\n\(c)")
 
         }
+        
+        print(plistLoad("testPlist"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func plistLoad(fileName:String) -> NSMutableDictionary{
+        let plistPath = NSBundle.mainBundle().pathForResource(fileName, ofType: "plist")
+        let data = NSMutableDictionary(contentsOfFile:plistPath!)
+        return data!
+    }
+    
+    func plistSave(fileName:String,content:[Dictionary<String,String>]){
+        let plistPath = NSBundle.mainBundle().pathForResource(fileName, ofType: "plist")
+        var data = NSMutableDictionary(contentsOfFile:plistPath!)
+        
+        for value in content{
+            data?.setValue(value["content"], forKey: value["id"]!)
+            print(data)
+        }
+        
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray
+        let documentsDirectory = paths.objectAtIndex(0) as! NSString
+        let path = documentsDirectory.stringByAppendingPathComponent("testPlist.plist")
+        data!.writeToFile(plistPath!, atomically: false)
+        let resultDictionary = NSMutableDictionary(contentsOfFile: path)
+        print("Saved testPlist.plist file is --> \(resultDictionary?.description)")
+        
     }
 
 
